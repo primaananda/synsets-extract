ef8e87e993707d51cef9065974eb28f95d53e371
primaananda <emailiputuprimaananda@gmail.com>
2018-08-01 23:25:36 +0700

    extract new synset

--- main.py
+++ main.py
@@ -206,4 +206,95 @@ if __name__ == '__main__':
 
     #====================
 
-    list_kata2 = ['abang', 'kakak', 'kakang']
+    list_kata2 = ['abang', 'kakak', 'kakang', 'kangmas', 'mas', 'uda', 'abrasi',
+                  'erosi', 'pengikisan', 'absen', 'bolos', 'mangkir', 'adidaya', 'adikuasa',
+                  'adipati', 'bupati', 'tumenggung', 'adiraja', 'kaisar', 'maharaja',
+                  'aduk', 'advokasi', 'pembelaan', 'apologi', 'pledoi', 'akomodasi', 'aksara',
+                  'alwah', 'ampelas', 'amunisi', 'anekdot', 'angsuran', 'cicilan', 'kredit',
+                  'anteng', 'antup', 'sengat', 'anyelir', 'arai', 'manggar', 'mayang', 'aras',
+                  'arit', 'pengarsipan', 'arteri', 'nadi', 'pengasahan', 'asam', 'asan',
+                  'asap', 'gas', 'atak', 'atensi', 'atlas', 'peta', 'atlet', 'olahragawan',
+                  'atom', 'zarah', 'molekul', 'atrium', 'auditor', 'awan', 'gegana', 'bom',
+                  'mega', 'awi', 'buluh', 'ayam', 'ayun']
+
+    synset_dataset = []
+
+    print()
+    synset_dataset.append(alt_gen('abang', open('datatest/dataset/1.json')))
+    synset_dataset.append(alt_gen('kakak', open('datatest/dataset/2.json')))
+    synset_dataset.append(alt_gen('kakang', open('datatest/dataset/3.json')))
+    synset_dataset.append(alt_gen('kangmas', open('datatest/dataset/4.json')))
+    synset_dataset.append(alt_gen('mas', open('datatest/dataset/5.json')))
+    synset_dataset.append(alt_gen('uda', open('datatest/dataset/6.json')))
+    synset_dataset.append(alt_gen('abrasi', open('datatest/dataset/7.json')))
+    synset_dataset.append(alt_gen('erosi', open('datatest/dataset/8.json')))
+    synset_dataset.append(alt_gen('pengikisan', open('datatest/dataset/9.json')))
+    synset_dataset.append(alt_gen('absen', open('datatest/dataset/10.json')))
+    synset_dataset.append(alt_gen('bolos', open('datatest/dataset/11.json')))
+    synset_dataset.append(alt_gen('mangkir', open('datatest/dataset/12.json')))
+    synset_dataset.append(alt_gen('adidaya', open('datatest/dataset/13.json')))
+    synset_dataset.append(alt_gen('adikuasa', open('datatest/dataset/14.json')))
+    synset_dataset.append(alt_gen('adipati', open('datatest/dataset/15.json')))
+    synset_dataset.append(alt_gen('bupati', open('datatest/dataset/16.json')))
+    synset_dataset.append(alt_gen('tumenggung', open('datatest/dataset/17.json')))
+    synset_dataset.append(alt_gen('adiraja', open('datatest/dataset/18.json')))
+    synset_dataset.append(alt_gen('kaisar', open('datatest/dataset/19.json')))
+    synset_dataset.append(alt_gen('maharaja', open('datatest/dataset/20.json')))
+    synset_dataset.append(alt_gen('aduk', open('datatest/dataset/21.json')))
+    synset_dataset.append(alt_gen('advokasi', open('datatest/dataset/22.json')))
+    synset_dataset.append(alt_gen('pembelaan', open('datatest/dataset/23.json')))
+    synset_dataset.append(alt_gen('apologi', open('datatest/dataset/24.json')))
+    synset_dataset.append(alt_gen('pledoi', open('datatest/dataset/25.json')))
+    synset_dataset.append(alt_gen('akomodasi', open('datatest/dataset/26.json')))
+    synset_dataset.append(alt_gen('aksara', open('datatest/dataset/27.json')))
+    synset_dataset.append(alt_gen('alwah', open('datatest/dataset/28.json')))
+    synset_dataset.append(alt_gen('ampelas', open('datatest/dataset/29.json')))
+    synset_dataset.append(alt_gen('amunisi', open('datatest/dataset/30.json')))
+    synset_dataset.append(alt_gen('anekdot', open('datatest/dataset/31.json')))
+    synset_dataset.append(alt_gen('angsuran', open('datatest/dataset/32.json')))
+    synset_dataset.append(alt_gen('cicilan', open('datatest/dataset/33.json')))
+    synset_dataset.append(alt_gen('kredit', open('datatest/dataset/34.json')))
+    synset_dataset.append(alt_gen('anteng', open('datatest/dataset/35.json')))
+    synset_dataset.append(alt_gen('antup', open('datatest/dataset/36.json')))
+    synset_dataset.append(alt_gen('sengat', open('datatest/dataset/37.json')))
+    synset_dataset.append(alt_gen('anyelir', open('datatest/dataset/38.json')))
+    synset_dataset.append(alt_gen('arai', open('datatest/dataset/39.json')))
+    synset_dataset.append(alt_gen('manggar', open('datatest/dataset/40.json')))
+    synset_dataset.append(alt_gen('mayang', open('datatest/dataset/41.json')))
+    synset_dataset.append(alt_gen('aras', open('datatest/dataset/42.json')))
+    synset_dataset.append(alt_gen('arit', open('datatest/dataset/43.json')))
+    synset_dataset.append(alt_gen('pengarsipan', open('datatest/dataset/44.json')))
+    synset_dataset.append(alt_gen('arteri', open('datatest/dataset/45.json')))
+    synset_dataset.append(alt_gen('nadi', open('datatest/dataset/46.json')))
+    synset_dataset.append(alt_gen('pengasahan', open('datatest/dataset/47.json')))
+    synset_dataset.append(alt_gen('asam', open('datatest/dataset/48.json')))
+    synset_dataset.append(alt_gen('asan', open('datatest/dataset/49.json')))
+    synset_dataset.append(alt_gen('asap', open('datatest/dataset/50.json')))
+    synset_dataset.append(alt_gen('gas', open('datatest/dataset/51.json')))
+    synset_dataset.append(alt_gen('atak', open('datatest/dataset/52.json')))
+    synset_dataset.append(alt_gen('atensi', open('datatest/dataset/53.json')))
+    synset_dataset.append(alt_gen('atlas', open('datatest/dataset/54.json')))
+    synset_dataset.append(alt_gen('peta', open('datatest/dataset/55.json')))
+    synset_dataset.append(alt_gen('atlet', open('datatest/dataset/56.json')))
+    synset_dataset.append(alt_gen('olahragawan', open('datatest/dataset/57.json')))
+    synset_dataset.append(alt_gen('atom', open('datatest/dataset/58.json')))
+    synset_dataset.append(alt_gen('zarah', open('datatest/dataset/59.json')))
+    synset_dataset.append(alt_gen('molekul', open('datatest/dataset/60.json')))
+    synset_dataset.append(alt_gen('atrium', open('datatest/dataset/61.json')))
+    synset_dataset.append(alt_gen('auditor', open('datatest/dataset/62a.json')))
+    synset_dataset.append(alt_gen('awan', open('datatest/dataset/63.json')))
+    synset_dataset.append(alt_gen('gegana', open('datatest/dataset/64.json')))
+    synset_dataset.append(alt_gen('bom', open('datatest/dataset/65.json')))
+    synset_dataset.append(alt_gen('mega', open('datatest/dataset/66.json')))
+    synset_dataset.append(alt_gen('awi', open('datatest/dataset/67.json')))
+    synset_dataset.append(alt_gen('buluh', open('datatest/dataset/68.json')))
+    synset_dataset.append(alt_gen('ayam', open('datatest/dataset/69.json')))
+    synset_dataset.append(alt_gen('ayun', open('datatest/dataset/70.json')))
+
+    f = open('datatest/output.txt', 'w+')
+    countd = 0
+    for k in synset_dataset:
+        output_synset = ('kata - ', list_kata2[countd], ' memiliki synset : ' ,k)
+        f.write(str(output_synset) + '\n')
+        countd += 1
+    f.close()
